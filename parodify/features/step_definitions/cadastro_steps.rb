9e4c9004429cf957ff1871e94effba0bc77e4611
Fernanda Antunes <fernanda.modeneze@gmail.com>
2020-10-22 22:53:09 -0300

    Consumindo a API

--- parodify/features/step_definitions/cadastro_steps.rb
+++ parodify/features/step_definitions/cadastro_steps.rb
@@ -5,6 +5,9 @@ Dado('que acesso a página de cadastro') do
   
   Quando('submeto o meu cadastro com:') do |table|
     user = table.rows_hash
+
+    delorean user[:email]
+
     find("input[name*=email]").set user[:email]
     find("input[placeholder='Sua senha secreta']").set user[:senha]
     find("input[placeholder='Confirme a senha']").set user[:senha_confirma]
