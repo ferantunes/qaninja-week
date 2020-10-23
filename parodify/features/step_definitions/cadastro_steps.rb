36f421557dabe39d3c3e67b385a85e7445a30828
Fernanda Antunes <fernanda.modeneze@gmail.com>
2020-10-22 22:25:42 -0300

    Preenchendo campos

--- parodify/features/step_definitions/cadastro_steps.rb
+++ parodify/features/step_definitions/cadastro_steps.rb
@@ -1,10 +1,13 @@
 Dado('que acesso a página de cadastro') do
     visit "http://parodify.qaninja.com.br"
+    click_on "EXPERIMENTE AGORA"
   end
   
   Quando('submeto o meu cadastro com:') do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    pending # Write code here that turns the phrase above into concrete actions
+    user = table.rows_hash
+    find("input[name*=email]").set user[:email]
+    find("input[placeholder='Sua senha secreta']").set user[:senha]
+    find("input[placeholder='Confirme a senha']").set user[:senha_confirma]
   end
   
   Então('devo ser redirecionado para a área logada') do
